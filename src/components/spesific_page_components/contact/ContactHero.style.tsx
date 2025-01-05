4d85d94787204237e84bf9d321d0ee37cc9070ec
NikosDigalakis <nikos_23_chania@hotmail.com>
2025-01-05 15:13:29 +0100

    updated contact hero img and deleted an unused img

--- src/components/spesific_page_components/contact/ContactHero.style.tsx
+++ src/components/spesific_page_components/contact/ContactHero.style.tsx
@@ -5,18 +5,18 @@ import ContactHeroImage from "../../images/holidayz-contact-croped.png";
 
 const StyledContactHero = styled.div`
     width:100%;
-    height: 700px;
+    height: 640px;
     background-image: url(${ContactHeroImage}); 
     background-size: cover; 
     background-repeat: no-repeat;
     background-position: center; 
 
-        @media (max-width: 768px) {
-        height: 500px;
+    @media (max-width: 768px) {
+        height: 400px;
     }
 
     @media (max-width: 480px) {
-        height: 300px;
+        height: 250px;
     }
 
 `;
