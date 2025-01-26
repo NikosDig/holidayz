6d5271fccd3245926139775f5dcb783fb5248e06
NikosDigalakis <nikos_23_chania@hotmail.com>
2025-01-26 14:05:29 +0100

    pathing to hero image

--- src/components/styled-components/contact/ContactHero.style.tsx
+++ src/components/styled-components/contact/ContactHero.style.tsx
@@ -3,10 +3,12 @@ import ContactHeroImage from "../../images/holidayz-contact-croped.png";
 
 
 
+
+
 const StyledContactHero = styled.div`
     width:100%;
     height: 640px;
-    background-image: url(${ContactHeroImage}); 
+    background-image: url(${ContactHeroImage});  
     background-size: cover; 
     background-repeat: no-repeat;
     background-position: center; 
